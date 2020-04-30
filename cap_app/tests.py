aaa526e8efb948ba988d53656e40f3b638e23ad9
Bishal Gautam <bisalgt@gmail.com>
2020-04-30 23:45:24 +0545

    adding unit test

--- cap_app/tests.py
+++ cap_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_url(self):
+        self.assertEqual(self.response.status_code, 200)
+        
--- cap_app/urls.py
+++ cap_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from cap_app.views import home
 
-app_name='cap_app'
 
 urlpatterns = [
     path('', home, name='home')
